c3a3ca065c18bae43bc243f88354eba5bd6bf033
raodz <raodziem@gmail.com>
2024-02-14 16:51:02 +0100

    deleting unnecessery files

---
+++ scripts/preprocessing.py
@@ -0,0 +1,15 @@
+from models.prepro import *
+
+data_file = 'train.csv'
+
+data = load_data(data_file)
+
+state_holiday_mapping = {'0': 0, 'a': 1, 'b': 1, 'c': 1}
+# All types of holidays are mapped into 1
+data = map_into_numeric(data, {'StateHoliday': state_holiday_mapping})
+
+data = data.groupby('Date').mean()
+
+assert is_data_full(data)
+
+
