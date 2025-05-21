de322f97d70f80cd0fd0e982bc52ab8204e50bca
raodz <raodziem@gmail.com>
2025-05-21 08:29:51 +0200

    feat: simplify main.py

--- main.py
+++ main.py
@@ -1,73 +1,23 @@
-import os
-import yaml
-import logging
-from src.data import preprocess_data
-from src.constants import RESULTS_DIR_NAME
-from models.arima_model import ARIMAModel
-from models.linear_regression_model import LinearRegressionModel
-from models.dummy_model import DummyModel
-from src.setup_logging import setup_logging
-from src.plots import plot_test_predictions
+from src.experiments import (get_experiment_definitions, execute_experiments,
+                             prepare_environment)
+from src.logging import (generate_summary, generate_model_comparison,
+                         log_program_complete)
+from src.utils import load_config
 
 
 def main():
-    with open('config/config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+    prepare_environment()
 
-    results_dir_path = os.path.join('..', RESULTS_DIR_NAME)
-    os.makedirs(results_dir_path, exist_ok=True)
-    setup_logging(results_dir_path)
-    logging.info("Program execution started")
+    config = load_config()
 
-    logging.info("Preparing data")
-    X_train, y_train, X_test, y_test = preprocess_data(config['data_path'],
-                                                       y_col=config['data_params']['y_col'],
-                                                       prc_samples_for_test=config['data_params']['prc_samples_for_test'],
-                                                       numeric_mappings=config['data_params']['numeric_mappings'])
-    logging.info("Data preparation completed")
+    experiment_definitions = get_experiment_definitions()
 
-    models = {
-        'ARIMA': ARIMAModel(config),
-        'Linear Regression': LinearRegressionModel(config),
-        'Dummy Model': DummyModel(config)
-    }
+    all_results = execute_experiments(config, experiment_definitions)
 
-    rmse_results = {}
-    predictions = {}
+    generate_summary(all_results)
+    generate_model_comparison(all_results)
 
-    for name, model in models.items():
-        logging.info(f"Training {name} model")
-        model.fit(X_train, y_train)
-        logging.info(f"{name} model trained")
-
-        rmse = model.evaluate(X_test, y_test)
-        rmse_results[name] = rmse
-        predictions[name] = model.predict(X_test)
-        logging.info(f"RMSE for {name} model: {rmse}")
-
-    logging.info("Comparing model performance:")
-    for name, rmse in rmse_results.items():
-        logging.info(f"{name}: RMSE = {rmse}")
-
-    # Create plot only for ARIMA model
-    y_pred = predictions['ARIMA']
-    plot_path = os.path.join(
-        results_dir_path,
-        config['plot_params']['title']
-    )
-    plot_title = config['plot_params']['title']
-    plot_figsize = config['plot_params']['figsize']
-
-    plot_test_predictions(
-        y_test,
-        y_pred,
-        save_path=plot_path,
-        title=plot_title,
-        figsize=plot_figsize
-    )
-    logging.info(f"Plot for ARIMA model saved at {plot_path}")
-
-    logging.info("Program execution completed")
+    log_program_complete()
 
 
 if __name__ == '__main__':
